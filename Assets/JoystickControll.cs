a0a0bcaa3ac74c380714fa721ccb01f4aa3d5f90
Pavel <ap@adverine.com>
2015-09-08 19:23:10 +0300

    FSM

--- Assets/JoystickControll.cs
+++ Assets/JoystickControll.cs
@@ -8,8 +8,8 @@ public class JoystickControll : MonoBehaviour {
 	public Text _debText = null;
 	string[] _controllers;
 	bool _connected = false;
-
-	// Use this for initialization
+	bool _iosFound = false;
+	
 	void Start () {
 		if (_debText == null) {
 			_debText = GameObject.Find("DebText").GetComponent<Text>();
@@ -17,16 +17,29 @@ public class JoystickControll : MonoBehaviour {
 
 		StartCoroutine (CheckJoustick());
 	}
-	
-	// Update is called once per frame
+
+	public void FoundIOSJoystick(){
+		_iosFound = true;
+	}
+
+	public void LostiOSJoystick(){
+		_iosFound = true;
+	}
+
 	void Update () {
 		string _tmp = "[Joystick input test]";
 
 		_tmp += "\nJoustick: ";
-		_tmp += (_connected) ? _controllers[0] : " not connected."; 
+		if (!_connected){
+			_tmp += " not connected."; 
+		}else{
+			_tmp += _controllers[0];
+		}
 
-		_tmp += "\nHorizontal: " + Input.GetAxis("Horizontal");
-		_tmp += "\nVertical: " + Input.GetAxis("Vertical");
+		_tmp += "\niOS J: " + _iosFound.ToString();
+
+		_tmp += "\nVertival: " + Input.GetAxis("Horizontal");
+		_tmp += "\nHorizontal: " + Input.GetAxis("Vertical");
 
 		_debText.text = _tmp;
 	}
@@ -34,10 +47,11 @@ public class JoystickControll : MonoBehaviour {
 	IEnumerator CheckJoustick(){
 		while (true) {
 			_controllers = Input.GetJoystickNames();
+
 			if (!_connected && _controllers.Length > 0){
 				_connected = true;
 			}
-			else if (_connected && _controllers.Length == 0){
+			else if (_connected == true && _controllers.Length == 0){
 				_connected = false;
 			}
 
